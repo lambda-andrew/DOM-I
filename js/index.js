c57607f39b9ed000c0f325f20f60346f9342fc18
Cory Hedeen <hedeencory@gmail.com>
2019-04-15 17:22:32 -0400

    Finished the anchor and made some progress on the top content. The image will not display despite using the exact syntax from the example. The h1 and button element display after using textContent despite them not being created elements but the anchor elements seem to need to be created elements.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,64 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav Bar
+let navigationBar = document.querySelector('header nav');
+let navAnchor = document.querySelector('header nav a');
+
+// Nav Items
+let newNavAnchor = document.createElement('a');
+newNavAnchor.textContent = "Services"
+newNavAnchor.style.color = "green"
+newNavAnchor.style.margin = "0 4%"
+navAnchor.appendChild(newNavAnchor);
+let newNavTwo = document.createElement('a');
+newNavTwo.textContent = "Product"
+newNavTwo.style.color = "green"
+newNavTwo.style.margin = "0 4%"
+navAnchor.appendChild(newNavTwo);
+let newNavThree = document.createElement('a');
+newNavThree.textContent = "Vision"
+newNavThree.style.color = "green"
+newNavThree.style.margin = "0 4%"
+navAnchor.appendChild(newNavThree);
+let newNavFour = document.createElement('a');
+newNavFour.textContent = "Features"
+newNavFour.style.color = "green"
+newNavFour.style.margin = "0 4%"
+navAnchor.appendChild(newNavFour);
+let newNavFive = document.createElement('a');
+newNavFive.textContent = "About"
+newNavFive.style.color = "green"
+newNavFive.style.margin = "0 4%"
+navAnchor.appendChild(newNavFive);
+let newNavSix = document.createElement('a');
+newNavSix.textContent = "Contact"
+newNavSix.style.color = "green"
+newNavSix.style.margin = "0 4%"
+navAnchor.appendChild(newNavSix);
+let newNavSeven = document.createElement('a');
+newNavSeven.textContent = "Goods"
+newNavSeven.style.color = "green"
+newNavSeven.style.margin = "0 4%"
+navAnchor.appendChild(newNavSeven);
+let newNavEight = document.createElement('a');
+newNavEight.textContent = "FAQ"
+newNavEight.style.color = "green"
+newNavEight.style.margin = "0 4%"
+navAnchor.appendChild(newNavEight);
+
+//Top content
+let topContent = document.querySelector('.cta');
+let wordContent = document.querySelector('.cta-text');
+let bigDom = document.querySelector('h1');
+bigDom.textContent = "DOM is Awesome"
+wordContent.appendChild(bigDom);
+let button = document.querySelector('button');
+button.textContent = "Get Started"
+wordContent.appendChild(button);
+let bigImage = document.getElementbyId("cta-img");
+bigImage.setAttribute('src', siteContent["cta"]["img-src"])
+
+// Middle Content
+let mainContent = document.querySelector('.main-content');
