309175a31fdc3a61cfe4c02542d3d4b4d0277366
Cory Hedeen <hedeencory@gmail.com>
2019-04-15 18:27:59 -0400

    If I comment out all previous work until the middle content, the image actually shows up.

--- js/index.js
+++ js/index.js
@@ -46,58 +46,58 @@ let navigationBar = document.querySelector('header nav');
 let navAnchor = document.querySelector('header nav a');
 
 // Nav Items
-let newNavAnchor = document.createElement('a');
-newNavAnchor.textContent = "Services"
-newNavAnchor.style.color = "green"
-newNavAnchor.style.margin = "0 4%"
-navAnchor.appendChild(newNavAnchor);
-let newNavTwo = document.createElement('a');
-newNavTwo.textContent = "Product"
-newNavTwo.style.color = "green"
-newNavTwo.style.margin = "0 4%"
-navAnchor.appendChild(newNavTwo);
-let newNavThree = document.createElement('a');
-newNavThree.textContent = "Vision"
-newNavThree.style.color = "green"
-newNavThree.style.margin = "0 4%"
-navAnchor.appendChild(newNavThree);
-let newNavFour = document.createElement('a');
-newNavFour.textContent = "Features"
-newNavFour.style.color = "green"
-newNavFour.style.margin = "0 4%"
-navAnchor.appendChild(newNavFour);
-let newNavFive = document.createElement('a');
-newNavFive.textContent = "About"
-newNavFive.style.color = "green"
-newNavFive.style.margin = "0 4%"
-navAnchor.appendChild(newNavFive);
-let newNavSix = document.createElement('a');
-newNavSix.textContent = "Contact"
-newNavSix.style.color = "green"
-newNavSix.style.margin = "0 4%"
-navAnchor.appendChild(newNavSix);
-let newNavSeven = document.createElement('a');
-newNavSeven.textContent = "Goods"
-newNavSeven.style.color = "green"
-newNavSeven.style.margin = "0 4%"
-navAnchor.appendChild(newNavSeven);
-let newNavEight = document.createElement('a');
-newNavEight.textContent = "FAQ"
-newNavEight.style.color = "green"
-newNavEight.style.margin = "0 4%"
-navAnchor.appendChild(newNavEight);
+// let newNavAnchor = document.createElement('a');
+// newNavAnchor.textContent = "Services"
+// newNavAnchor.style.color = "green"
+// newNavAnchor.style.margin = "0 4%"
+// navAnchor.appendChild(newNavAnchor);
+// let newNavTwo = document.createElement('a');
+// newNavTwo.textContent = "Product"
+// newNavTwo.style.color = "green"
+// newNavTwo.style.margin = "0 4%"
+// navAnchor.appendChild(newNavTwo);
+// let newNavThree = document.createElement('a');
+// newNavThree.textContent = "Vision"
+// newNavThree.style.color = "green"
+// newNavThree.style.margin = "0 4%"
+// navAnchor.appendChild(newNavThree);
+// let newNavFour = document.createElement('a');
+// newNavFour.textContent = "Features"
+// newNavFour.style.color = "green"
+// newNavFour.style.margin = "0 4%"
+// navAnchor.appendChild(newNavFour);
+// let newNavFive = document.createElement('a');
+// newNavFive.textContent = "About"
+// newNavFive.style.color = "green"
+// newNavFive.style.margin = "0 4%"
+// navAnchor.appendChild(newNavFive);
+// let newNavSix = document.createElement('a');
+// newNavSix.textContent = "Contact"
+// newNavSix.style.color = "green"
+// newNavSix.style.margin = "0 4%"
+// navAnchor.appendChild(newNavSix);
+// let newNavSeven = document.createElement('a');
+// newNavSeven.textContent = "Goods"
+// newNavSeven.style.color = "green"
+// newNavSeven.style.margin = "0 4%"
+// navAnchor.appendChild(newNavSeven);
+// let newNavEight = document.createElement('a');
+// newNavEight.textContent = "FAQ"
+// newNavEight.style.color = "green"
+// newNavEight.style.margin = "0 4%"
+// navAnchor.appendChild(newNavEight);
 
 //Top content
-let topContent = document.querySelector('.cta');
-let wordContent = document.querySelector('.cta-text');
-let bigDom = document.querySelector('h1');
-bigDom.textContent = "DOM is Awesome"
-wordContent.appendChild(bigDom);
-let button = document.querySelector('button');
-button.textContent = "Get Started"
-wordContent.appendChild(button);
-let bigImage = document.getElementbyId("cta-img");
-bigImage.setAttribute('src', siteContent["cta"]["img-src"])
+// let topContent = document.querySelector('.cta');
+// let wordContent = document.querySelector('.cta-text');
+// let bigDom = document.querySelector('h1');
+// bigDom.textContent = "DOM is Awesome"
+// wordContent.appendChild(bigDom);
+// let button = document.querySelector('button');
+// button.textContent = "Get Started"
+// wordContent.appendChild(button);
+// let bigImage = document.getElementbyId("cta-img");
+// bigImage.setAttribute('src', siteContent["cta"]["img-src"])
 
 // Middle Content
 let midContent = document.querySelector('.main-content');
@@ -105,4 +105,4 @@ let featuresHeader = document.createElement('h4');
 featuresHeader.textContent = "Features"
 midContent.appendChild(featuresHeader);
 let midImage = document.getElementById('middle-img');
-midImage.setAttribute('src', siteContent["main-content"]["img/mid-page-accent.jpg"])
+midImage.setAttribute('src', "img/mid-page-accent.jpg");
