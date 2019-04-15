99a467df1867e509383adbdddfb39b103274306e
efrain <efarinva98@gmail.com>
2019-04-15 19:01:46 -0400

    finished with tags

--- js/index.js
+++ js/index.js
@@ -47,4 +47,79 @@ pic.setAttribute("src", siteContent["cta"]["img-src"]);
 const circle_pic = document.getElementById("middle-img");
 circle_pic.setAttribute("src",siteContent["main-content"] ["middle-img-src"]);
 
+// pitctures
+const mContent = document.querySelector(".cta-text");
+mContent.textContent = siteContent["cta"]["h1"];
 
+
+// const mButton = document.querySelector(".cta-text");
+// mButton.textContent = siteContent["cta"]["button"];
+
+const nav1 = document.querySelector("a");
+nav1.textContent = siteContent["nav"]["nav-item-1"];
+// const nav2 = document.querySelector("a");
+// nav2.textContent = siteContent["nav"]["nav-item-2"];
+// const nav3 = document.querySelectorAll("a");
+// nav3.textContent = siteContent["nav"]["nav-item-3"];
+// const nav4 = document.querySelectorAll("a");
+// nav4.textContent = siteContent["nav"]["nav-item-4"];
+// const nav5 = document.querySelectorAll("a");
+// nav5.textContent = siteContent["nav"]["nav-item-5"];
+// const nav6 = document.querySelectorAll("a");
+// nav6.textContent = siteContent["nav"]["nav-item-6"];
+
+
+const feat = document.querySelector(".main-content .top-content .text-content h4");
+feat.textContent = siteContent["main-content"]["features-h4"];
+// feat.style.display = "flex";
+// feat.style.justifyContent = "space-between";
+
+const featP = document.querySelector(".main-content .top-content .text-content p");
+featP.textContent = siteContent["main-content"]["features-content"];
+// featP.style.display = "flex";
+// featP.style.justifyContent = "space-between";
+
+const about = document.querySelector(".main-content .top-content .text-content h4");
+about.textContent = siteContent["main-content"]["about-h4"];
+// about.style.display = "flex";
+// about.style.justifyContent = "space-between";
+
+const aboutP = document.querySelector(".main-content .top-content .text-content p");
+aboutP.textContent = siteContent["main-content"]["about-content"];
+// aboutP.style.display = "flex";
+// aboutP.style.justifyContent = "space-between";
+
+
+//top
+
+const ser = document.querySelector(".main-content .bottom-content .text-content h4");
+ser.textContent = siteContent["main-content"]["services-h4"];
+const serP = document.querySelector(".main-content .bottom-content .text-content p");
+serP.textContent = siteContent["main-content"]["services-content"];
+
+const pro = document.querySelector(".main-content .bottom-content .text-content h4");
+pro.textContent = siteContent["main-content"]["product-h4"];
+const proP = document.querySelector(".main-content .bottom-content .text-content p");
+proP.textContent = siteContent["main-content"]["product-content"];
+
+const vis = document.querySelector(".main-content .bottom-content .text-content h4");
+vis.textContent = siteContent["main-content"]["vision-h4"];
+const visP = document.querySelector(".main-content .bottom-content .text-content p");
+visP.textContent = siteContent["main-content"]["vision-content"];
+//bottom
+
+const con = document.querySelector(".contact h4");
+con.textContent = siteContent["contact"]["contact-h4"];
+
+const add = document.querySelector(".contact p");
+add.textContent = siteContent["contact"]["address"];
+
+const phone = document.querySelector(".contact p");
+phone.textContent = siteContent["contact"]["phone"];
+
+
+const email = document.querySelector(".contact p");
+email.textContent = siteContent["contact"]["email"];
+
+const copy = document.querySelector("footer");
+copy.textContent = siteContent["footer"]["copyright"];
