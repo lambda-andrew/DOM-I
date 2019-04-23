8a470f863707e634e9bcfdac30bec8c65affaf4c
kingtamaaj <kingtamaaj@gmail.com>
2019-04-23 17:09:39 -0400

    Cta, main, middle and bottom content. Contacts and Nav yet to complete

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ const siteContent = {
   "main-content": {
     "features-h4":"Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    
     "about-h4":"About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
@@ -39,4 +40,43 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+
+//Header/NAV
+
+//CTA Image
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"] ["img-src"]);
+
+//CTA Text
+let h1 = document.getElementsByTagName("h1");
+h1[0].textContent = siteContent.cta.h1;
+
+//CTA Button
+let btn = document.getElementsByTagName('button');
+btn[0].textContent = siteContent.cta.button;
+
+// Main-Content 
+let h4 = document.getElementsByTagName('h4');
+h4[0].textContent = siteContent['main-content']['features-h4'];
+h4[1].textContent = siteContent['main-content']['about-h4'];
+
+// Main-Content Text
+let p = document.getElementsByTagName('p');
+p[0].textContent = siteContent['main-content']['features-content'];
+p[1].textContent = siteContent['main-content']['about-content'];
+
+// Middle Img/Main-Content
+ let midImg = document.querySelector('#middle-img');
+ midImg.setAttribute('src', siteContent ['main-content']['middle-img-src']);
+
+// Bottom-Content
+h4[2].textContent = siteContent['main-content']['services-h4'];
+h4[3].textContent = siteContent['main-content']['product-h4'];
+h4[4].textContent = siteContent['main-content']['vision-h4'];
+
+// Bottom-Content Text
+p[2].textContent = siteContent['main-content']['services-content'];
+p[3].textContent = siteContent['main-content']['product-content'];
+p[4].textContent = siteContent['main-content']['vision-content'];
