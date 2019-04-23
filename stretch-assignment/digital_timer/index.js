e6d4f3066c28a9ddfa2399222ea096ca0d455720
Cory Hedeen <hedeencory@gmail.com>
2019-04-23 13:20:14 -0400

    Github issues but anyway, stopwatch complete

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,59 @@
+let digits = document.querySelectorAll('.digit');
+let digitContainer = document.querySelector('.digits')
+let tens = document.getElementById('secondTens');
+let littleTens = document.getElementById('secondOnes');
+let bigSeconds = document.getElementById('msHundreds');
+let littleSeconds = document.getElementById('msTens');
+let button = document.createElement('button');
+button.textContent = "start"
+button.style.margin = "100px"
+digitContainer.appendChild(button)
+let buttonTwo = document.createElement('button');
+buttonTwo.textContent = "stop"
+buttonTwo.style.margin = "100px"
+digitContainer.appendChild(buttonTwo)
+
+button.addEventListener('click', () => timer())
+
+
+function timer(){
+
+  tens.textContent = 0;
+  littleTens.textContent = 0;
+  bigSeconds.textContent = 0;
+  littleSeconds.textContent = 0;
+
+let firstHand = setInterval(function(){tens.textContent = (tens.textContent * 1) + 1}, 10000);
+setInterval(function(){if((tens.textContent * 1) === 10){tens.textContent = 0}})
+
+
+let secondHand = setInterval(function(){littleTens.textContent = (littleTens.textContent * 1) + 1}, 1000);
+setInterval(function(){if((littleTens.textContent * 1) === 9){color()}})
+setInterval(function(){if((littleTens.textContent * 1) === 10){stopClock()}})
+
+
+let thirdHand = setInterval(function(){bigSeconds.textContent = (bigSeconds.textContent * 1) + 1}, 100);
+setInterval(function(){if((bigSeconds.textContent * 1) === 10){bigSeconds.textContent = 0}})
+
+
+
+let fourthHand = setInterval(function(){littleSeconds.textContent = (littleSeconds.textContent * 1) + 1}, 50);
+setInterval(function(){if((littleSeconds.textContent * 1) === 10){littleSeconds.textContent = 0}})
+
+function color(){
+    digits.forEach(item => item.style.color = "red")
+}
+
+buttonTwo.addEventListener('click', () => stopClock())
+function stopClock(){
+  clearInterval(firstHand);
+  tens.textContent = 0;
+  clearInterval(secondHand);
+  littleTens.textContent = 0;
+  clearInterval(thirdHand);
+  bigSeconds.textContent = 0;
+  clearInterval(fourthHand);
+  littleSeconds.textContent = 0;
+  digits.forEach(item => item.style.color = "black")
+}
+}
